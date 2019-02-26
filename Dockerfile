60c2fb6c8f8956c3bca69341838d81fd00adde79
Weller Marcel <Marcel.Weller@novatec-gmbh.de>
2019-02-26 16:13:38 +0100

    improved perdict.py

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,13 @@ FROM python:3
 
 ADD lrcnModel.py /
 
+RUN pip install numpy
+RUN pip install pandas
+RUN pip install matplotlib
+RUN pip install os
+RUN pip install pathlib
+RUN pip install PIL
+RUN pip install sklearn
+RUN pip install keras
+
 CMD [ "python", "./lrcnModel.py" ]
--- predict.py
+++ predict.py
@@ -4,22 +4,51 @@ import matplotlib.pyplot as plt
 import os
 
 from pathlib import Path
-from PIL import Image as PIL_Image
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.externals import joblib
-from keras.models import Sequential
-from keras.layers.wrappers import TimeDistributed
 from keras.preprocessing import sequence
-from keras.preprocessing.image import load_img, img_to_array
-from keras.layers.core import Dense, Dropout, Flatten, Activation
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers.recurrent import LSTM
-from keras.layers.pooling import GlobalAveragePooling1D
 from keras.utils import plot_model
+from keras.models import load_model
 
-encoderPath         = Path('DeepLearningModel/Encoder/oneHotEncoder.pkl')
-videoDataPath       = Path('F:/Datasets/20bn-jester-v1/')
-testMetaDataPath    = Path('F:/Datasets/jester-v1-test.csv')
-modelSavePath       = Path('DeepLearningModel/Model/model.h5')
+encoderPath         = Path('mwe/oneHotEncoder.pkl')
+videoDataPath       = Path('dataset/20bn-jester-v1/')
+testMetaDataPath    = Path('dataset/jester-v1-test.csv')
+modelSavePath       = Path('mwe/model.h5')
 
-model.load(modelSavePath)
+prediction_limit = 5
+batchsize = 32
+
+
+def data_generator(featurePath, labelPath, batchsize, labelEncoder):
+    metaData = pd.read_csv(labelPath, delimiter=';', header=None, index_col=0, names=['gesture'])
+    videoIdIter = iter(metaData.index.values)
+
+    while True:
+        features = []
+        labels = []
+
+        while len(labels) < batchsize:
+            videoId = next(videoIdIter)
+            videoDirectory = featurePath / str(videoId)
+            if len(os.listdir(videoDirectory)) >= NUM_FRAMES: # don't use if video length is too small
+                video = []
+                for imageCounter, image in enumerate(os.listdir(videoDirectory), 1):
+                    if imageCounter > NUM_FRAMES: break # break if video length is too big
+                    image_values = PIL_Image.open(os.path.join(videoDirectory, image))
+                    image_values = image_values.convert('RGB') # L: converts to greyscale | RGB
+                    image_values = image_values.resize((IMG_WIDTH, IMG_HEIGHT), PIL_Image.ANTIALIAS)
+                    video.append(np.asarray(image_values))
+
+                features.append(np.asarray(video))
+                labels.append(metaData.at[videoId, 'gesture'])
+
+        features = np.asarray(features)
+        labels = labelEncoder.transform(np.asarray(labels).reshape(-1, 1))
+        yield(features, labels)
+
+oneHotEncoder = joblib.load(encoderPath)
+model = load_model(modelSavePath)
+test_batches = data_generator(videoDataPath, testMetaDataPath, batchsize, oneHotEncoder)
+predict = model.predict_generator(test_batches, steps=prediciton_limit)
+
+#toDo: Evaluation
